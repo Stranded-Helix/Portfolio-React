97fe04ce8c076a625977eb14e39bf1bdc1ff1426
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2021-07-26 17:32:40 -0500

    Updates

---
+++ precache-manifest.3fff16276f9907eadb2fc2f7cb40ee5d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ddba6aec692132ce473b07d7b0a4bf",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "0e87d5555bdb23350e1b",
+    "url": "/Portfolio-React/static/css/2.25bb6d0f.chunk.css"
+  },
+  {
+    "revision": "1d72410a7f1af03c10a2",
+    "url": "/Portfolio-React/static/css/main.905f8f33.chunk.css"
+  },
+  {
+    "revision": "0e87d5555bdb23350e1b",
+    "url": "/Portfolio-React/static/js/2.ea75f700.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/Portfolio-React/static/js/2.ea75f700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d72410a7f1af03c10a2",
+    "url": "/Portfolio-React/static/js/main.4d8ae650.chunk.js"
+  },
+  {
+    "revision": "2effeafa6a821fc346de",
+    "url": "/Portfolio-React/static/js/runtime-main.5acc9983.js"
+  },
+  {
+    "revision": "7a9d3e71ae4e2f0c5a8b119e4ae9324d",
+    "url": "/Portfolio-React/static/media/Barcode-generator.7a9d3e71.png"
+  },
+  {
+    "revision": "a8a03c5c8659bee506b88dabf719ccd7",
+    "url": "/Portfolio-React/static/media/BudgetTracker.a8a03c5c.png"
+  },
+  {
+    "revision": "4c2cab48ac5fe4f245329ac981caddd2",
+    "url": "/Portfolio-React/static/media/G.Fite Resume - 2021.4c2cab48.pdf"
+  },
+  {
+    "revision": "bde87ac5ce5c5452206c972695079265",
+    "url": "/Portfolio-React/static/media/GlobalNibzHomeImage.bde87ac5.png"
+  },
+  {
+    "revision": "4a54f7a427108581074acad273ab91a8",
+    "url": "/Portfolio-React/static/media/Portfolio.4a54f7a4.jpg"
+  },
+  {
+    "revision": "89692d27f552ee4cae039cf404e2ceba",
+    "url": "/Portfolio-React/static/media/SilverHelix.89692d27.png"
+  },
+  {
+    "revision": "889c4771fb37940be7a9c412db51474c",
+    "url": "/Portfolio-React/static/media/StolenCanvasHomeImage.889c4771.png"
+  },
+  {
+    "revision": "d23213313a9cecff7f23270c7688e942",
+    "url": "/Portfolio-React/static/media/TechBlogScreenshot.d2321331.png"
+  },
+  {
+    "revision": "5754df3220d253892eccbcb3639055bd",
+    "url": "/Portfolio-React/static/media/WeatherDashboard.5754df32.png"
+  },
+  {
+    "revision": "cb1fcce0661b223d5d3755537990b321",
+    "url": "/Portfolio-React/static/media/WebsiteBackground.cb1fcce0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aabecd2b5116e1c7791e5c25dc296075.js"
+  "/Portfolio-React/precache-manifest.3fff16276f9907eadb2fc2f7cb40ee5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
